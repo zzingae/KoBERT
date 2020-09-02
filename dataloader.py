063154a05d7ef61ee0f865638a3bccde8e7f37b2
home <zzingae@dongguk.edu>
2020-09-03 02:07:34 +0900

    working on fine-tuning chatbot

---
+++ dataloader.py
@@ -0,0 +1,74 @@
+import torch
+from torch.utils.data import Dataset
+from gluonnlp.data import SentencepieceTokenizer
+from kobert.utils import get_tokenizer
+import pandas as pd
+
+
+def padding_tokens(tokens, maxlen):
+    #Preprocessing the text to be suitable for BERT
+    tokens = ['[CLS]'] + tokens + ['[SEP]'] #Insering the CLS and SEP token in the beginning and end of the sentence
+    if len(tokens) < maxlen:
+        return tokens + ['[PAD]' for _ in range(maxlen - len(tokens))] #Padding sentences
+    else:
+        return tokens[:maxlen-1] + ['[SEP]'] #Prunning the list to be of specified max length
+
+class QnADataset(Dataset):
+    def __init__(self, filename, vocab, maxlen):
+
+        #Store the contents of the file in a pandas dataframe
+        self.df = pd.read_csv(filename, header=0, encoding='utf-8')
+        # self.df = pd.read_csv(filename, delimiter = '\t')
+        self.sp = SentencepieceTokenizer(get_tokenizer())
+        self.vocab = vocab
+        self.maxlen = maxlen
+
+        self.sp.tokens.index('!')
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index):
+
+        #Selecting the sentence and label at the specified index in the data frame
+        question = self.df.loc[index, 'Q']
+        answer = self.df.loc[index, 'A']
+        # label = self.df.loc[index, 'label']
+
+        qtoks = self.sp(question)
+        atoks = self.sp(answer)
+
+        qtoks = padding_tokens(qtoks, self.maxlen)
+        atoks = padding_tokens(atoks, self.maxlen)
+
+        qids = [self.vocab.token_to_idx[t] for t in qtoks]
+        qids_tensor = torch.tensor(qids) #Converting the list to a pytorch tensor
+
+        aids = [self.vocab.token_to_idx[t] for t in atoks]
+        aids_tensor = torch.tensor(aids) #Converting the list to a pytorch tensor
+
+        #Obtaining the attention mask i.e a tensor containing 1s for no padded tokens and 0s for padded ones
+        attn_mask = (qids_tensor != 0).long()
+
+        # seq, attn_masks, tgt, tgt_mask
+        return qids_tensor, attn_mask, aids_tensor
+
+
+class Batch:
+    "Object for holding a batch of data with mask during training."
+    def __init__(self, src, trg=None, pad=0):
+        self.src = src
+        self.src_mask = (src != pad).unsqueeze(-2)
+        if trg is not None:
+            self.trg = trg[:, :-1]
+            self.trg_y = trg[:, 1:]
+            self.trg_mask = \
+                self.make_std_mask(self.trg, pad)
+            self.ntokens = (self.trg_y != pad).data.sum()
+    
+    # @staticmethod
+    # def make_std_mask(tgt, pad):
+    #     "Create a mask to hide padding and future words."
+    #     tgt_mask = (tgt != pad).unsqueeze(-2)
+    #     tgt_mask = tgt_mask & Variable(
+    #         subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
+    #     return tgt_mask
--- train.py
+++ train.py
@@ -1,42 +1,40 @@
 from model import make_model
-from gluonnlp.data import SentencepieceTokenizer
-from kobert.utils import get_tokenizer
+from dataloader import QnADataset
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
-class Batch:
-    "Object for holding a batch of data with mask during training."
-    def __init__(self, src, trg=None, pad=0):
-        self.src = src
-        self.src_mask = (src != pad).unsqueeze(-2)
-        if trg is not None:
-            self.trg = trg[:, :-1]
-            self.trg_y = trg[:, 1:]
-            self.trg_mask = \
-                self.make_std_mask(self.trg, pad)
-            self.ntokens = (self.trg_y != pad).data.sum()
-    
-    @staticmethod
-    def make_std_mask(tgt, pad):
-        "Create a mask to hide padding and future words."
-        tgt_mask = (tgt != pad).unsqueeze(-2)
-        tgt_mask = tgt_mask & Variable(
-            subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data))
-        return tgt_mask
 
-def train(net, criterion, opti, train_loader, val_loader, args):
+def get_accuracy_from_logits(logits, labels):
+    probs = F.softmax(logits, dim=1)
+    output = torch.argmax(probs, dim=1)
+    #Convert probabilities to predictions, 1 being positive and 0 being negative
+    #Check which predictions are the same as the ground truth and calculate the accuracy
+    acc = (output == labels).float().mean()
+    return acc
 
-    for ep in range(args.max_eps):
-        for it, (seq, attn_masks, labels) in enumerate(train_loader):
+def train(model, criterion, opti, train_loader):
+
+    max_eps = 2
+    gpu = 'cuda'
+    print_every = 10
+
+    for ep in range(max_eps):
+        for it, (question, attn_masks, answer) in enumerate(train_loader):
             #Clear gradients
             opti.zero_grad()  
             #Converting these to cuda tensors
-            seq, attn_masks, labels = seq.cuda(args.gpu), attn_masks.cuda(args.gpu), labels.cuda(args.gpu)
+            # question, attn_masks, answer = question.cuda(gpu), attn_masks.cuda(gpu), answer.cuda(gpu)
 
             #Obtaining the logits from the model
-            output = net(seq, attn_masks)
-            logits = net.generator(output)
+            tgt_mask = attn_masks
+            output = model(question, attn_masks, answer, tgt_mask)
+            logits = model.generator(output)
 
             #Computing loss
-            loss = criterion(logits.squeeze(-1), labels.float())
+            loss = criterion(logits.squeeze(-1), answer.float())
 
             #Backpropagating the gradients
             loss.backward()
@@ -44,15 +42,23 @@ def train(net, criterion, opti, train_loader, val_loader, args):
             #Optimization step
             opti.step()
 
-            if (it + 1) % args.print_every == 0:
-                acc = get_accuracy_from_logits(logits, labels)
+            if (it + 1) % print_every == 0:
+                acc = get_accuracy_from_logits(logits, answer)
                 print("Iteration {} of epoch {} complete. Loss : {} Accuracy : {}".format(it+1, ep+1, loss.item(), acc))
 
 
-# tmp_model, vocab = make_model(N=3)
+model, vocab = make_model(3)
+criterion = nn.BCEWithLogitsLoss()
+opti = optim.Adam(model.parameters(), lr = 2e-5)
 
-tok_path = get_tokenizer()
-sp  = SentencepieceTokenizer(tok_path)
-print(sp('한국어 모델을 공유합니다.'))
-print(vocab)
+# Creating instances of training and validation set
+maxlen=25
+path='./data/ChatBotData.csv'
+train_set = QnADataset(path, vocab, maxlen)
+# train_Q, eval_Q, train_A, eval_A = train_test_split(question, answer, test_size=0.33, random_state=42)
+# Creating instances of training and validation dataloaders
+num_workers=5
+train_loader = DataLoader(train_set, batch_size = 64)
+# val_loader = DataLoader(val_set, batch_size = 64, num_workers = 5)
 
+train(model,criterion,opti,train_loader)
